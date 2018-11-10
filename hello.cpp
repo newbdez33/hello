fb24b4d7ba9bde648b01da012ab4cb513c5e2f78
Jacky <newbdez33@gmail.com>
2018-11-10 11:21:20 +0900

    cdt v1.4.0

--- hello.cpp
+++ hello.cpp
@@ -2,15 +2,13 @@
 #include <eosiolib/print.hpp>
 using namespace eosio;
 
-class [[eosio::contract("hashhubdemo1")]] hello : public eosio::contract {
+CONTRACT hello : public eosio::contract {
   public:
       using contract::contract;
 
-      [[eosio::action]]
-      void hi( name user ) {
-         print( "Hello, ", name{user} );
-         require_recipient( user );
-      }
+      ACTION hi( name user ) {
+	      print_f( "Hello % from hello", user );
+	  }
 };
 
 EOSIO_DISPATCH( hello, (hi) )
